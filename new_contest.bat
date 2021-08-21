912fcb95c78799949b405f38911be9b7cefeec0d
h1j1k1 <annihilation0504@gmail.com>
2021-08-22 04:18:22 +0900

    new_contest.batを置くだけで使えるように整備しました

--- new_contest.bat
+++ new_contest.bat
@@ -1,5 +1,13 @@
 @echo off
 setlocal enabledelayedexpansion
+
+rem * Released under the Eclipse Public v1.0 license.
+rem * see https://www.eclipse.org/legal/epl-v10.html
+
+echo Copyright (c) 2021 https://twitter.com/tea_creats
+echo page: https://github.com/tea-create/my_atcoder/blob/main/new_contest.bat
+echo Released under the Eclipse Public v1.0  license.
+echo.
 cd /d %~dp0
 goto main
 
@@ -7,9 +15,9 @@ goto main
 ;AtCoder
 ;////////////////////////////////////
 :AtCoder
-cd ./Atcoder/
+if not exist Atcoder mkdir AtCoder
+cd ./AtCoder/
     SET /P Contest_kind="Contest_kinda[ABC, ARC, AGC, others]:"
-    SET /P cid="contest_ID[xxx]:"
     if /i "!Contest_kind!" == "ABC" (
         goto ABC
     ) else if /i "!Contest_kind!" == "ARC" (
@@ -25,23 +33,37 @@ exit /b
 ;Atcoder_Contests
 ;//////////////////////////////////////
 :AC_Others
-cd ./others/
+    SET /P cid="contest_name[eg.typical90]:"
+    if not exist others mkdir others
+    cd ./others/
+    if not exist !cid! mkdir !cid!
+    cd ./!cid!/
+    SET /P Problem_Count="Problem_Count[0-25]:"
+    SET /a end=!Problem_Count!+65
+    for /l %%n in (65,1,!end!) do (
+        cmd /c exit /b %%n
+        set fn=!=ExitCodeAscii!.cpp
+        echo //new_created: > !fn!
+    )
+    pause
+    exit /b
 :ABC
 :ARC
 :AGC
-    mkdir !Contest_kind!
+    SET /P cid="contest_ID[xxx]:"
+    if not exist !Contest_kind! mkdir !Contest_kind!
     cd ./!Contest_kind!/
 	if exist !cid! (
 		goto end
 	)
-    mkdir !cid!
+    if not exist !cid! mkdir !cid!
     cd ./!cid!/
-    echo //new_created: > A.cpp
-    echo //new_created: > B.cpp
-    echo //new_created: > C.cpp
-    echo //new_created: > D.cpp
-    echo //new_created: > E.cpp
-    echo //new_created: > F.cpp
+    SET /a end=8+65
+    for /l %%n in (65,1,!end!) do (
+        cmd /c exit /b %%n
+        set fn=!=ExitCodeAscii!.cpp
+        echo //new_created: > !fn!
+    )
     mkdir Build
     mkdir Release
     mkdir debug
@@ -50,7 +72,8 @@ exit /b
 ;//////////////////////////////////////
 ;Yukicoder
 ;//////////////////////////////////////
-:Yukicoder
+:yukicoder
+    if not exist yukicoder mkdir yukicoder
     cd ./yukicoder/
     set /p pnum="Problem_Number[first problem's number]:"
     set /p pcnt="Promlem_Count[how many problems?]:"
@@ -69,21 +92,22 @@ exit /b
 ;CodeForces
 ;//////////////////////////////////////
 :CodeForces
+	if not exist CodeForces mkdir CodeForces
     cd ./CodeForces/
     set /P cid="Contest_ID[xxxx]:"
 	if exist !cid! (
 		goto end
 	)
-    mkdir !cid!
+    if not exist !cid! mkdir !cid!
     cd ./!cid!/
-    mkdir problem
+    if not exist problem mkdir problem
     cd problem
-    echo //new_created: > A.cpp
-    echo //new_created: > B.cpp
-    echo //new_created: > C.cpp
-    echo //new_created: > D.cpp
-    echo //new_created: > E.cpp
-    echo //new_created: > F.cpp
+    SET /a end=6+65
+    for /l %%n in (65,1,!end!) do (
+        cmd /c exit /b %%n
+        set fn=!=ExitCodeAscii!.cpp
+        echo //new_created: > !fn!
+    )
     mkdir Build
     mkdir Release
     mkdir debug
@@ -94,18 +118,18 @@ exit /b
 ;main
 ;//////////////////////////////////////
 :main
-
-SET /P Con="contest[Atcoder, YukiCoder, CodeForces]:"
-cd ./contest/
-echo !Con!
-if /i "!Con!" == "Atcoder" (
-    goto Atcoder
-) else if /i "!Con!" == "YukiCoder" (
-    goto Yukicoder
-) else if /i "!Con!" == "CodeForces" (
-    goto CodeForces
-)
-exit /b
+	SET /P Con="contest[Atcoder, YukiCoder, CodeForces]:"
+	if not exist contest mkdir contest
+	cd ./contest/
+	echo !Con!
+	if /i "!Con!" == "Atcoder" (
+		goto Atcoder
+	) else if /i "!Con!" == "YukiCoder" (
+		goto Yukicoder
+	) else if /i "!Con!" == "CodeForces" (
+		goto CodeForces
+	)
+	exit /b
 
 :end
 exit /b
